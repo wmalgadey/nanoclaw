2eabb0f069a8f086270c0a4b8395235d83c4c288
Wolfgang Malgadey <wolfgang@malgadey.de>
2026-04-03 10:13:31 +0200

    added container directories to tailscale plugin

--- src/plugins/tailscale.ts
+++ src/plugins/tailscale.ts
@@ -3,6 +3,7 @@ import { registerPlugin } from './registry.js';
 registerPlugin({
   name: 'tailscale',
   containerEnvKeys: ['TAILSCALE_AUTH_KEY', 'TAILSCALE_HOSTNAME'],
+  containerDirectories: ['/var/run/tailscale'],
   binaryInstall: {
     archive: 'https://pkgs.tailscale.com/stable/tailscale_1.96.4_amd64.tgz',
     extract: ['tailscale', 'tailscaled'],
